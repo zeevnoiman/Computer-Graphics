f98c8fada41ed8a9a8e0eda35938f29a507230ae
EliMoser <Lipamoser@gmail.com>
2017-06-08 19:44:06 +0300

    test comment machon wi fi

--- graphicProject/src/Tests/EmmissionTest.java
+++ graphicProject/src/Tests/EmmissionTest.java
@@ -14,22 +14,25 @@ public class EmmissionTest {
 
 	@Test
 	public void emmissionSemple() {
-		
+		//test comment machon w
 		Scene scene = new Scene(new AmbientLight(255,255,255), new Color(0,0,0), new Camera(), 100);
 		ImageWriter imageWriter = new ImageWriter("PointLightTest", 500, 500, 500, 500);
-		Sphere sph1 = new Sphere(400, new Point3D(0,0,-1000));
+		Sphere sph1 = new Sphere(400, new Point3D(100,50,-1000));
 		Sphere sph2 = new Sphere(300, new Point3D(500,-300,-700));
 		Sphere sph3 = new Sphere(200, new Point3D(0,-250,-500));
 		Triangle tri = new Triangle(new Point3D(400, 2400,-1200), new Point3D(1101,200,-1300), new Point3D(300,-300,-1101));
 		Triangle tri5 = new Triangle(new Point3D(-400, 2000,-1100), new Point3D(200,-100,-1200), new Point3D(-500,-700,-900));
+		Triangle tri3 = new Triangle(new Point3D(-700, 4000,-1500), new Point3D(200,-100,-1200), new Point3D(-500,-700,-900));
 		sph1.setShininess(35);
-		sph1.setEmmission(new Color(17,15,116));
-		sph2.setShininess(35);
-		sph2.setEmmission(new Color(17,15,116));
-		sph3.setShininess(35);
-		sph3.setEmmission(new Color(0,100,0));
+		sph1.setEmmission(new Color(10,15,250));
+		sph2.setShininess(20);
+		sph2.setEmmission(new Color(17,200,200));
+		sph3.setShininess(5);
+		sph3.setEmmission(new Color(255,200,56));
+		tri3.setShininess(200);
+		tri3.setEmmission(new Color(255,0,0));
 		tri.setShininess(10);
-		tri.setEmmission(new Color(30,144,255));
+		tri.setEmmission(new Color(30,12,255));
 		tri5.setShininess(10);
 		tri5.setEmmission(new Color(30,144,255));
 		PointLight pointLight = new PointLight(new Color(255,50,50), new Point3D(220,200,-20), 0.002, 0.002, 0.001);
@@ -37,6 +40,7 @@ public class EmmissionTest {
 		scene.addGeometry(sph2);
 		scene.addGeometry(sph3);
 		scene.addGeometry(tri);
+		scene.addGeometry(tri3);
 		scene.addGeometry(tri5);
 		scene.addLight(pointLight);
 		Render render = new Render(imageWriter, scene);
